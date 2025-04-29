d20c507cd471547008efaded5faae9dc6ce2f45e
ProObito <ahmedumaid707@gmail.com>
2025-04-29 12:32:53 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,45 +1,46 @@
-from datetime import datetime
-from pytz import timezone
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
+import asyncio
+import time
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import Config
-from aiohttp import web
-from route import web_server
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            name="renamer",
+            name="AutoRenamerBot",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
-            workers=200,
+            workers=50,  # Reduced for bot-hosting.net
             plugins={"root": "plugins"},
             sleep_threshold=15,
         )
+        self.start_time = time.time()
 
     async def start(self):
         await super().start()
         me = await self.get_me()
         self.mention = me.mention
-        self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
-        if Config.WEBHOOK:
-            app = web.AppRunner(await web_server())
-            await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 8080).start()     
-        print(f"{me.first_name} ɪꜱ ꜱᴛᴀʀᴛᴇᴅ.....⚡️")
-        for id in Config.ADMIN:
-            try: await self.send_message(id, f"**{me.first_name} ɪꜱ ꜱᴛᴀʀᴛᴇᴅ.....⚡️**")                                
-            except: pass
-        if Config.LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
-            except:
-                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
+        self.username = me.username
+        print(f"{me.first_name} is started... ✨")
 
-Bot().run()
+        # Notify log channel
+        uptime = str(timedelta(seconds=int(time.time() - self.start_time)))
+        try:
+            await self.send_photo(
+                chat_id=Config.LOG_CHANNEL,
+                photo=Config.START_PIC,
+                caption=f"**Bot is restarted!**\n\nUptime: `{uptime}`",
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton("Updates", url="https://t.me/animes_sub_society")]]
+                ),
+            )
+        except Exception as e:
+            print(f"Failed to send message to log channel: {e}")
+
+    async def stop(self):
+        await super().stop()
+        print("Bot stopped.")
+
+if __name__ == "__main__":
+    Bot().run()
--- config.py
+++ config.py
@@ -1,92 +1,170 @@
-import re, os, time
+import re
+import os
+import time
+from os import environ
+from datetime import timedelta
 
-id_pattern = re.compile(r'^.\d+$') 
+id_pattern = re.compile(r'^\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", 20718334)
+    API_ID    = os.environ.get("API_ID", "20718334")
     API_HASH  = os.environ.get("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7932515290:AAErP6vIZw6JuI79RN2pJohjLfnDMCTjSEY")
-   
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7830743177:AAHkVvb0AwI-bDqa7O0JUZdb_tvSdS4E0fA") 
+
     # database config
-    DB_NAME = os.environ.get("DB_NAME","Cluster0")
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://spxsolo:umaid2008@cluster0.7fbux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
- 
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")     
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://obito:umaid2008@cluster0.engyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    PORT = os.environ.get("PORT", "8080")
+
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/dcd4fca8a27c510455683-50a0b682b91412149d.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5585016974').split()]
-    FORCE_SUB_1 = os.environ.get("FORCE_SUB_1", "animes_sub_society")
-    FORCE_SUB_2 = os.environ.get("FORCE_SUB_2", "ongoiing_society")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002015354644))
-    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002015354644"))
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5585016974 7328629001').split()]
+    FORCE_SUB = os.environ.get('FORCE_SUB', 'animes_sub_society').split(',')
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002200709110"))
+    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002200709110"))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5585016974"))
+    MAX_CONCURRENT = int(os.environ.get("MAX_CONCURRENT", "5"))
 
-    # wes response configuration     
-    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+class Txt:
+    START_TXT = """<b>Hey! {}  
 
+» I am an advanced rename bot! Which can autorename your files with custom caption and thumbnail and also sequence them perfectly</b>"""
 
+    FILE_NAME_TXT = """<b>» <u>Setup auto rename format</u></b>
 
-class Txt(object):
-    # part of text configuration
-    START_TXT = """
-<b>ʜᴇʏ {}!✨
+<b>Variables :</b>
+➲ episode - to replace episode number  
+➲ season - to replace season number  
+➲ quality - to replace quality  
 
-🫧 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇᴅ ʀᴇɴᴀᴍᴇ ʙᴏᴛ!
-ᴡʜɪᴄʜ ᴄᴀɴ ᴍᴀɴᴜᴀʟʟʏ ʀᴇɴᴀᴍᴇ ʏᴏᴜʀ ғɪʟᴇs ᴡɪᴛʜ ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ ᴀɴᴅ ᴛʜᴜᴍʙɴᴀɪʟ ᴀɴᴅ ᴀʟsᴏ ᴄᴀɴ sᴇᴛ ᴘʀᴇғɪx ᴀɴᴅ sᴜғғɪx ᴏɴ ʏᴏᴜʀ ғɪʟᴇs.⚡️
+<b>‣ For ex:- </b> `/autorename Overflow [Sseason Eepisode] - [Dual] quality`
 
-✨ ᴛʜɪs ʙᴏᴛ ɪs ᴄʀᴇᴀᴛᴇᴅ ʙʏ : <a href=https://t.me/i_killed_my_clan>❰⏤͟͞ 𝚯𝗕𝗜𝗧𝗢 -//-❱</a>
-──────────────────
-๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴏᴡ ᴛᴏ ᴜsᴇ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.</b>
-"""
+<b>‣ /Autorename: rename your media files by including 'episode' and 'quality' variables in your text, to extract episode and quality present in the original filename.</b>"""
 
-    DONATE_TXT = """<b>
-👋 ʜᴇʏ ᴛʜᴇʀᴇ {},
+    ABOUT_TXT = """<b>❍ My name: <a href="https://t.me/animes_sub_society">Auto Rename</a>
+❍ Developer: <a href="https://t.me/cosmic_freak">Yato</a>
+❍ Github: <a href="https://github.com/cosmic_freak">Yato</a>
+❍ Language: <a href="https://www.python.org/">Python</a>
+❍ Database: <a href="https://www.mongodb.com/">Mongo DB</a>
+❍ Main channel: <a href="https://t.me/animes_sub_society">Anime Sub Society</a></b>"""
 
-Jᴜsᴛ ᴡᴀɴᴛᴇᴅ ᴛᴏ ᴅʀᴏᴘ ᴀ ǫᴜɪᴄᴋ ᴛʜᴀɴᴋs ʏᴏᴜʀ ᴡᴀʏ! Iɴ ᴏᴜʀ ᴛɪɴʏ ᴄᴏʀɴᴇʀ ᴏғ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ʙᴏᴛs, ʜᴀᴠɪɴɢ ʏᴏᴜʀ sᴜᴘᴘᴏʀᴛ ғᴇᴇʟs ʟɪᴋᴇ ɢᴇᴛᴛɪɴɢ ᴀ ᴡᴀʀᴍ ʜᴜɢ.
+    THUMBNAIL_TXT = """<b><u>» To set custom thumbnail</u></b>
+    
+➲ /start: send any photo to automatically set it as a thumbnail..
+➲ /del_thumb: use this command to delete your old thumbnail.
+➲ /view_thumb: use this command to view your current thumbnail.
 
-Nᴏ ɴᴇᴇᴅ ᴛᴏ sᴛʀᴇss ᴀʙᴏᴜᴛ ᴅᴏɴᴀᴛɪᴏɴs – ʏᴏᴜʀ ʟɪᴛᴛʟᴇ sᴜᴘᴘᴏʀᴛ ᴀɴᴅ ᴄʟɪᴄᴋs ᴍᴇᴀɴ ᴛʜᴇ ᴡᴏʀʟᴅ ᴛᴏ ᴜs.
+Note: if no thumbnail saved in bot then, it will use thumbnail of the original file to set in renamed file"""
 
-Bɪɢ ᴛʜᴀɴᴋs ғᴏʀ ʙᴇɪɴɢ ᴛʜᴇ sᴜᴘᴘᴏʀᴛ sᴜᴘᴇʀsᴛᴀʀ ɪɴ ᴏᴜʀ sᴍᴀʟʟ, ʙᴜᴛ ᴀᴡᴇsᴏᴍᴇ, sᴘᴀᴄᴇ!🌟</b>"""
+    CAPTION_TXT = """<b><u>» To set custom caption and media type</u></b>
+    
+<b>Variables :</b>         
+size: {filesize}
+duration: {duration}
+filename: {filename}
 
-    HELP_TXT = """<b>ᴏʙɪᴛᴏ ʀᴇɴᴀᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs🫧
- 
-ᴏʙɪᴛᴏ ʀᴇɴᴀᴍᴇ ʙᴏᴛ ɪꜱ ᴀ ᴠᴇʀʏ ʜᴀɴᴅʏ ᴀɴᴅ ʜᴇʟᴘғᴜʟ ʙᴏᴛ  ᴛʜᴀᴛ ʜᴇʟᴘꜱ ʏᴏᴜ ʀᴇɴᴀᴍᴇ ᴀɴᴅ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ꜰɪʟᴇꜱ ᴇꜰꜰᴏʀᴛʟᴇꜱꜱʟʏ.
+➲ /set_caption: to set a custom caption.
+➲ /see_caption: to view your custom caption.
+➲ /del_caption: to delete your custom caption.
 
-<u>ɪᴍᴘᴏʀᴛᴀɴᴛ ғᴇᴀᴛᴜʀᴇs:</u>
-➲ ᴄᴀɴ ʀᴇɴᴀᴍᴇ ᴀɴʏ ғɪʟᴇs.
-➲ ᴄᴀɴ ᴍᴀɴᴀɢᴇ ᴍᴇᴛᴀᴅᴀᴛᴀ.
-➲ ᴜᴘʟᴏᴀᴅ ɪɴ ᴅᴇsɪʀᴇ ᴍᴇᴅɪᴀ ᴛʏᴘᴇ.
-➲ ᴄᴀɴ sᴇᴛ ᴄᴜsᴛᴏᴍ ᴘʀᴇғɪx & sᴜғғɪx.
-➲ ʀᴇɴᴀᴍᴇ ғɪʟᴇs ᴠᴇʀʏ ǫᴜɪᴄᴋʟʏ.
-</b>  
-"""
+» For ex:- /set_caption File Name: {filename}"""
 
-#⚠️ Dᴏɴ'ᴛ Rᴇᴍᴏᴠᴇ Oᴜʀ Cʀᴇᴅɪᴛꜱ @GeekLuffy🙏🥲
-    ABOUT_TXT = """<b>
-» ᴅᴇᴠᴇʟᴏᴩᴇʀ : <a href=https://t.me/i_killed_my_clan>❰⏤͟͞ 𝚯𝗕𝗜𝗧𝗢 -//-❱</a>
-» ɢɪᴛʜᴜʙ :  <a href=https://t.me/i_killed_my_clan>❰⏤͟͞ 𝚯𝗕𝗜𝗧𝗢 -//-❱</a>
-» ʟɪʙʀᴀʀʏ : <a href=https://github.com/pyrogram>ᴘʏʀᴏɢʀᴀᴍ</a>
-» ʟᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>ᴘʏᴛʜᴏɴ</a>
-» ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/i_killed_my_clan>ᴏʙɪᴛᴏ ʀᴇɴᴀᴍᴇ ʙᴏᴛ</a>
-» ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/Animes_Sub_Society>ᴀɴɪᴍᴇ sᴏᴄɪᴇᴛʏ</a>
-» ᴍᴀɪɴ ɢʀᴏᴜᴘ : <a href=https://t.me/ahss_help_zone>sᴏᴄɪᴇᴛʏ ᴄʜᴀᴛ ᴢᴏɴᴇ</a></b>"""
+    PROGRESS_BAR = """\n
+<b>» Size</b> : {1} | {2}
+<b>» Done</b> : {0}%
+<b>» Speed</b> : {3}/s
+<b>» ETA</b> : {4} """
 
-    META_TXT = """
-**ᴍᴀɴᴀɢɪɴɢ ᴍᴇᴛᴀᴅᴀᴛᴀ ғᴏʀ ʏᴏᴜʀ ᴠɪᴅᴇᴏs ᴀɴᴅ ғɪʟᴇs**
+    DONATE_TXT = """<blockquote> Thanks for showing interest in donation</blockquote>
 
-**ᴠᴀʀɪᴏᴜꜱ ᴍᴇᴛᴀᴅᴀᴛᴀ:**
+<b><i>💞 If you like our bot feel free to donate any amount ₹10, ₹20, ₹50, ₹100, etc.</i></b>
 
-- **ᴛɪᴛʟᴇ**: Descriptive title of the media.
-- **ᴀᴜᴛʜᴏʀ**: The creator or owner of the media.
-- **ᴀʀᴛɪꜱᴛ**: The artist associated with the media.
-- **ᴀᴜᴅɪᴏ**: Title or description of audio content.
-- **ꜱᴜʙᴛɪᴛʟᴇ**: Title of subtitle content.
-- **ᴠɪᴅᴇᴏ**: Title or description of video content.
+Donations are really appreciated it helps in bot development
 
-**ᴄᴏᴍᴍᴀɴᴅꜱ ᴛᴏ ᴛᴜʀɴ ᴏɴ ᴏғғ ᴍᴇᴛᴀᴅᴀᴛᴀ:**
+ <u>You can also donate through UPI</u>
+
+ UPI ID : <code>LodaLassan@fam</code>
+
+If you wish you can send us ss
+on - @ProYato"""
+
+    PREMIUM_TXT = """<b>Upgrade to our premium service and enjoy exclusive features:
+○ Unlimited Renaming: rename as many files as you want without any restrictions.
+○ Early Access: be the first to test and use our latest features before anyone else.
+
+• Use /plan to see all our plans at once.
+
+➲ First step: pay the amount according to your favorite plan to this rohit162@fam UPI ID.
+
+➲ Second step: take a screenshot of your payment and share it directly here: @sewxiy 
+
+➲ Alternative step: or upload the screenshot here and reply with the /bought command.
+
+Your premium plan will be activated after verification</b>"""
+
+    PREPLANS_TXT = """<b>👋 bro,
+    
+🎖️ <u>Available plans</u> :
+
+Pricing:
+➜ Monthly premium: ₹50/month
+➜ Daily premium: ₹5/day
+➜ For bot hosting: contact @ProYato
+
+➲ UPI ID - <code>LodaLassan@fam</code>
+
+‼️Upload the payment screenshot here and reply with the /bought command.</b>"""
+
+    HELP_TXT = """<b>Here is help menu important commands:
+
+Awesome features🫧
+
+Rename bot is a handy tool that helps you rename and manage your files effortlessly.
+
+➲ /Autorename: auto rename your files.
+➲ /Metadata: commands to turn on off metadata.
+➲ /Help: get quick assistance.</b>"""
+
+    SEND_METADATA = """
+<b>--Metadata Settings:--</b>
+
 ➜ /metadata: Turn on or off metadata.
 
-**ᴄᴏᴍᴍᴀɴᴅꜱ ᴛᴏ ꜱᴇᴛ ᴍᴇᴛᴀᴅᴀᴛᴀ:**
+<b>Description</b> : Metadata will change MKV video files including all audio, streams, and subtitle titles."""
+
+    SOURCE_TXT = """
+<b>Hey,
+ This is auto rename bot,
+an open source telegram auto rename bot.</b>
+
+Written in python with the help of :
+[Pyrogram](https://github.com/pyrogram/pyrogram)
+[Python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot)
+And using [Mongo](https://cloud.mongodb.com) as database.
+
+<b>Here is my source code :</b> [Github](https://github.com/codeflix_bots/autorenamebot)
+
+Auto rename bot is licensed under the [MIT License](https://github.com/codeflix_bots/autorenamebot/blob/main/LICENSE).
+© 2024 | [Support Chat](https://t.me/animes_sub_society), all rights reserved."""
+
+    META_TXT = """
+**Managing metadata for your videos and files**
+
+**Various metadata:**
+
+- **Title**: Descriptive title of the media.
+- **Author**: The creator or owner of the media.
+- **Artist**: The artist associated with the media.
+- **Audio**: Title or description of audio content.
+- **Subtitle**: Title of subtitle content.
+- **Video**: Title or description of video content.
+
+**Commands to turn on off metadata:**
+➜ /metadata: Turn on or off metadata.
+
+**Commands to set metadata:**
 
 ➜ /settitle: Set a custom title of media.
 ➜ /setauthor: Set the author.
@@ -95,7 +173,7 @@ Bɪɢ ᴛʜᴀɴᴋs ғᴏʀ ʙᴇɪɴɢ ᴛʜᴇ sᴜᴘᴘᴏʀᴛ sᴜᴘᴇ�
 ➜ /setsubtitle: Set subtitle title.
 ➜ /setvideo: Set video title.
 
-**ᴇxᴀᴍᴘʟᴇ:** /settitle Your Title Here
+**Example:** /settitle Your Title Here
 
-**ᴜꜱᴇ ᴛʜᴇꜱᴇ ᴄᴏᴍᴍᴀɴᴅꜱ ᴛᴏ ᴇɴʀɪᴄʜ ʏᴏᴜʀ ᴍᴇᴅɪᴀ ᴡɪᴛʜ ᴀᴅᴅɪᴛɪᴏɴᴀʟ ᴍᴇᴛᴀᴅᴀᴛᴀ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ!**
+**Use these commands to enrich your media with additional metadata information!**
 """
